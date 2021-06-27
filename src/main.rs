b33566a6b5f96f507e3ba87e14018ec5fae83e49
nibon7 <nibon7@163.com>
2021-06-27 09:45:35 +0800

    use structopt to parse cmdline
    Signed-off-by: nibon7 <nibon7@163.com>

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,39 @@
-use anyhow::{Context, Result};
-use clap::{App, Arg};
+use anyhow::Result;
 use edid_gen::{CvtModeBuilder, Version};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "edid-gen", version = "0.1", about = "Generate EDID file")]
+struct Opt {
+    /// Timing name
+    #[structopt(short, long)]
+    timing_name: String,
+    /// Ouput file
+    #[structopt(short, long)]
+    output: String,
+    /// EDID version (v1.0 to v1.4)
+    #[structopt(short, long, default_value = "1.4")]
+    version: Version,
+    /// Create a mode with reduced blanking
+    #[structopt(short, long)]
+    reduced: bool,
+    /// Desired horizontal resolution (multiple of 8, required)
+    x: i32,
+    /// Desired vertical resolution (required)
+    y: i32,
+    /// Desired refresh rate
+    #[structopt(default_value = "60")]
+    refresh: i32,
+}
 
 fn main() -> Result<()> {
-    let matches = App::new("edid-gen")
-        .about("Generate EDID file")
-        .version("0.1")
-        .arg(
-            Arg::with_name("timing_name")
-                .long("timing-name")
-                .short("t")
-                .takes_value(true)
-                .help("Timing name")
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("output")
-                .long("output")
-                .short("o")
-                .takes_value(true)
-                .help("Output file")
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("edid_version")
-                .long("edid-version")
-                .short("v")
-                .takes_value(true)
-                .help("EDID version (v1.0 to v1.4)")
-                .default_value("1.4"),
-        )
-        .arg(
-            Arg::with_name("reduced")
-                .long("reduced")
-                .short("r")
-                .help("Create a mode with reduced blanking"),
-        )
-        .arg(
-            Arg::with_name("X")
-                .required(true)
-                .help("Desired horizontal resolution (multiple of 8, required)"),
-        )
-        .arg(
-            Arg::with_name("Y")
-                .required(true)
-                .help("Desired vertical resolution (required)"),
-        )
-        .arg(
-            Arg::with_name("refresh")
-                .help("Desired refresh rate")
-                .default_value("60"),
-        )
-        .get_matches();
-
-    let timing_name = matches
-        .value_of("timing_name")
-        .context("timing_name invalid")?;
-
-    let output = matches.value_of("output").context("output invalid")?;
-
-    let version = matches
-        .value_of("edid_version")
-        .context("edid version invalid")
-        .map(|s| s.parse::<Version>())??;
-
-    let reduced = matches.is_present("reduced");
-
-    let x = matches
-        .value_of("X")
-        .context("param X invalid")
-        .map(|s| s.parse::<i32>())??;
-
-    let y = matches
-        .value_of("Y")
-        .context("param Y invalid")
-        .map(|s| s.parse::<i32>())??;
-
-    let refresh = matches
-        .value_of("refresh")
-        .context("param refresh invalid")
-        .map(|s| s.parse::<i32>())??;
-
+    let opt: Opt = Opt::from_args();
     let mode = CvtModeBuilder::new()
-        .hdisplay(x)
-        .vdisplay(y)
-        .vrefresh(refresh)
-        .reduced(reduced)
+        .hdisplay(opt.x)
+        .vdisplay(opt.y)
+        .vrefresh(opt.refresh)
+        .reduced(opt.reduced)
         .build();
 
-    edid_gen::generate_edid(&mode, version, timing_name, output)
+    edid_gen::generate_edid(&mode, opt.version, &opt.timing_name, &opt.output)
 }
